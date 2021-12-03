383a4e0c4395e676c8aa87cb925782c4e213f76a
wesley <wesley.mprog@gmail.com>
2021-12-03 20:48:18 -0300

    refactor: Make the registration tables dynamic

--- src/app/components/cadastro/itens-tab/itens-tab.component.ts
+++ src/app/components/cadastro/itens-tab/itens-tab.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
+const itens = [
+  {
+    servico: "Serviço 1",
+    quantidade: 1,
+    valor: 10,
+    valorTotal: 10,
+  }
+];
+
 @Component({
   selector: 'app-itens-tab',
   templateUrl: './itens-tab.component.html',
@@ -7,9 +16,17 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class ItensTabComponent implements OnInit {
 
+  tableColumns = ['Serviço', 'Quantidade', 'Valor unitário',
+                  'Valor Total', 'Itens'];
+  items = itens;
+  totalValue = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items.forEach((i) => {
+      this.totalValue += i.valor;
+    });
   }
 
 }
--- src/app/components/cadastro/projetos-tab/projetos-tab.component.ts
+++ src/app/components/cadastro/projetos-tab/projetos-tab.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const projetos = [
+  {
+    nome: 'Projeto 1',
+    valorTotal: 10
+  },
+ 
+]
+
 @Component({
   selector: 'app-projetos-tab',
   templateUrl: './projetos-tab.component.html',
   styleUrls: ['./projetos-tab.component.scss']
 })
 export class ProjetosTabComponent implements OnInit {
+
+  tableColumns = ['Projeto', 'Valor Total']
+  projects = projetos;
 
   constructor() { }
 
